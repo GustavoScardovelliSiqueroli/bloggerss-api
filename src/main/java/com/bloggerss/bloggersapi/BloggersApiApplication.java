c9a871f5b2aff4e598f75907243058c54a31e66f
Gustavo Scardovelli Siqueroli <55260462+GustavoScardovelliSiqueroli@users.noreply.github.com>
2024-04-12 18:06:08 -0300

    config: Starting project

---
+++ src/main/java/com/bloggerss/bloggersapi/BloggersApiApplication.java
@@ -0,0 +1,13 @@
+package com.bloggerss.bloggersapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggersApiApplication.class, args);
+	}
+
+}
