4c0074c98dce0f2347d7b459e72c2ffadbca3186
Gustavo Scardovelli Siqueroli <55260462+GustavoScardovelliSiqueroli@users.noreply.github.com>
2024-04-13 14:50:30 -0300

    feat: Add post model

---
+++ src/main/java/com/bloggerss/bloggersapi/models/PostModel.java
@@ -0,0 +1,62 @@
+package com.bloggerss.bloggersapi.models;
+
+import java.io.Serializable;
+import java.sql.Date;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table(name = "post_tb")
+public class PostModel implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID postId;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String content;
+    @Temporal(TemporalType.DATE)
+    private Date created_at;
+
+    public UUID getPostId() {
+        return postId;
+    }
+
+    public void setPostId(UUID postId) {
+        this.postId = postId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+
+}
