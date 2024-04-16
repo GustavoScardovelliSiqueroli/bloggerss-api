5ad3e636da37b02bd0192ef6e43b792a0e08c716
Gustavo Scardovelli Siqueroli <55260462+GustavoScardovelliSiqueroli@users.noreply.github.com>
2024-04-16 18:53:27 -0300

    config: path /posts/.. now is public.

--- src/main/java/com/bloggerss/bloggersapi/config/SecurityConfig.java
+++ src/main/java/com/bloggerss/bloggersapi/config/SecurityConfig.java
@@ -2,21 +2,32 @@ package com.bloggerss.bloggersapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain postsFilterChain(HttpSecurity http) throws Exception {
+        http
+                .securityMatcher("/posts/**")
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.GET).permitAll()
+                )
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
